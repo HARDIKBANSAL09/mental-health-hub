769117d303eb13a42990dd696f7330a767bd8d17
HARDIKBANSAL09 <102210776+HARDIKBANSAL09@users.noreply.github.com>
2023-11-26 14:31:53 +0530

    Add files via upload

---
+++ src/modules/utils/validationSchema.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+
+const registerSchema = Joi.object({
+  username: Joi.string()
+    .min(3)
+    .max(21)
+    .required(),
+    
+   
+  email: Joi.string().email().lowercase().required(),
+  password: Joi.string().min(6).max(50).required()
+})
+
+const loginSchema = Joi.object({
+  username: Joi.string()
+    .min(3)
+    .max(21)
+    .required(),
+    
+    
+  password: Joi.string().min(6).max(50).required()
+})
+
+module.exports = { registerSchema, loginSchema }
